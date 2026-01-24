3019023458653c8626ba8c65b6a85697a800f05f
James Volpe <volpestyle@gmail.com>
2026-01-24 16:36:47 -0500

    Clean slate

--- packages/core/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./pipeline/engine.js";
-export * from "./pipeline/types.js";
-export * from "./providers/registry.js";
-export * from "./storage/local.js";
-export * from "./utils/spritesheet.js";
